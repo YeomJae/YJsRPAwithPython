9fc7840b31fe4cc04a4f1509d0ee59b8d072c783
SAHOON KIM <sahuni@gmail.com>
2024-11-14 14:28:53 +0900

    Create webProcess.py

---
+++ DAY07/webProcess.py
@@ -0,0 +1,10 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import util as ut
+
+def login_drvier() :
+  
+def set_attach_file() :
